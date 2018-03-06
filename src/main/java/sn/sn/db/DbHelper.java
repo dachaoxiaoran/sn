a91dd01ff1ca64604cde1628b2741c2d190eb47b
dachaoxiaoran <18649045364@163.com>
2018-03-06 10:32:53 +0800

    查询sql

--- src/main/java/sn/sn/db/DbHelper.java
+++ src/main/java/sn/sn/db/DbHelper.java
@@ -1,7 +1,13 @@
 package sn.sn.db;
 
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.PreparedStatement;
@@ -42,19 +48,41 @@ public class DbHelper {
 		return res;
 	}
 	
-//	public static void main(String[] args) {
-//		while (true) {
-//			Map<String, Double> map = new Currency().getData();
-//			String sql = "insert into rate(eurusd, usdjpy, gbpusd, usdcad, usdsek, usdchf, usdcny, usdx) values("
-//					+ map.get("eurusd") + ", "
-//					+ map.get("usdjpy") + ", "
-//					+ map.get("gbpusd") + ", "
-//					+ map.get("usdcad") + ", "
-//					+ map.get("usdsek") + ", "
-//					+ map.get("usdchf") + ", "
-//					+ map.get("usdcny") + ", "
-//					+ map.get("usdx") + ")";
-//			System.err.println(new DbHelper().insert(sql));
-//		}
-//	}
+	/**
+	 * select sql
+	 * @param sql
+	 * @return List<Map<String, String>>
+	 * @throws Throwable
+	 */
+	public List<Map<String, String>> select(String sql)throws Throwable {
+		List<Map<String, String>> list = new ArrayList<>();
+		Connection conn = null;
+		PreparedStatement pst = null;
+		try {
+			Class.forName(IConstant.MYSQL_NAME);
+			conn = (Connection) DriverManager.getConnection(IConstant.MYSQL_URL, IConstant.MYSQL_USER, IConstant.MYSQL_PASSWORD);
+			pst = (PreparedStatement) conn.prepareStatement(sql);
+			ResultSet resultSet = pst.executeQuery();
+			
+			ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+			String[] names = new String[resultSetMetaData.getColumnCount()];
+			for (int i = 1; i <= resultSetMetaData.getColumnCount(); i++) names[i - 1] = resultSetMetaData.getColumnName(i);
+			
+			while (resultSet.next()) {
+				Map<String, String> map = new HashMap<>();
+				for (int i = 0; i < names.length; i++) map.put(names[i], resultSet.getObject(i + 1).toString());
+				list.add(map);
+			}
+		} catch(Exception e) {
+			throw e;
+		} finally {
+			try {
+				if (pst != null) pst.close();
+				if (conn != null) conn.close();
+			} catch (SQLException e) {
+				throw e;
+			}
+		}
+		return list;
+	}
 }
